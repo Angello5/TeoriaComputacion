b1eb41cd3e09d2163f36ae70e103c08e5c5125b5
Angello 5 <angello.llerena@usil.pe>
2023-10-25 18:42:43 -0500

    correccion de errores, casi final

--- codigo.py
+++ codigo.py
@@ -6,7 +6,8 @@ def transiciones(estado, simbolo):
     elif estado == 'q1' and simbolo == 'C':
         return ['q4']
     elif estado == 'q1' and simbolo == 'G':
-        return ['q2', 'q4', 'q7']    
+        return ['q2', 'q4', 'q7']
+
     elif estado == 'q2' and simbolo == 'A':
         return ['q3', 'q6']
     elif estado == 'q2' and simbolo == 'T':
@@ -14,7 +15,8 @@ def transiciones(estado, simbolo):
     elif estado == 'q2' and simbolo == 'C':
         return ['q2', 'q4']
     elif estado == 'q2' and simbolo == 'G':
-        return ['q3']
+        return ['q3','q6']
+
     elif estado == 'q3' and simbolo == 'A':
         estado == ['q8']
     elif estado == 'q3' and simbolo == 'T':
@@ -23,6 +25,7 @@ def transiciones(estado, simbolo):
         estado == ['q8']
     elif estado == 'q3' and simbolo == 'G':
         estado ==[]
+
     elif estado == 'q4' and simbolo == 'A':
         estado == ['q5', 'q8']
     elif estado == 'q4' and simbolo == 'T':
@@ -31,24 +34,45 @@ def transiciones(estado, simbolo):
         estado == ['q4', 'q8']
     elif estado == 'q4' and simbolo == 'G':
         estado == ['q5','q8']
+
+    elif estado == 'q5' and simbolo == 'A':
+        estado == ['q3','q8']
+    elif estado == 'q5' and simbolo == 'T':
+        estado == ['q3','q8']
+    elif estado == 'q5' and simbolo == 'C':
+        estado == ['q3','q8']
     elif estado == 'q5' and simbolo == 'G':
         estado == ['q8']
-    elif estado == 'q5' and simbolo == 'A':
-        estado == ['q8','q3']
-    elif estado == 'q5' and simbolo == 'T':
-        estado == ['q8','q3']
-    elif estado == 'q5' and simbolo == 'T':
-        estado == ['q8','q3']
-    elif estado == 'q6' and simbolo == 'G':
-        estado == ['q8']
+
     elif estado == 'q6' and simbolo == 'A':
         estado == ['q8']
     elif estado == 'q6' and simbolo == 'T':
         estado == ['q8']
-    elif estado == 'q6' and simbolo == 'T':
+    elif estado == 'q6' and simbolo == 'C':
+        estado == ['q8']
+    elif estado == 'q6' and simbolo == 'G':
         estado == ['q8']
 
-def main():    
+    elif estado == 'q7' and simbolo == 'A':
+        estado == ['q7','q8']
+    elif estado == 'q7' and simbolo == 'T':
+        estado == ['q3','q8']
+    elif estado == 'q7' and simbolo == 'C':
+        estado == ['q7','q8']
+    elif estado == 'q7' and simbolo == 'G':
+        estado == ['q8']
+
+    elif estado == 'q8' and simbolo == 'A':
+        estado == [ ]
+    elif estado == 'q8' and simbolo == 'T':
+        estado == [ ]
+    elif estado == 'q8' and simbolo == 'C':
+        estado == [ ]
+    elif estado == 'q8' and simbolo == 'G':
+        estado == [ ]
+
+
+def main():
     estados = {'q1', 'q2', 'q3', 'q4', 'q5', 'q6', 'q7', 'q8'}
     alfabeto = {'A', 'T', 'C', 'G'}
     estado_inicial = 'q1'

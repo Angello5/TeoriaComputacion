91a3a04fc72d243666eacac89163e58439c9a502
Carlos Martin Quispe Bartolo <104172383+tripalidex@users.noreply.github.com>
2023-10-27 19:39:36 -0500

    Extracción de datos de un archivo FASTA v1

--- codigo.py
+++ codigo.py
@@ -78,7 +78,33 @@ def main():
     estado_inicial = 'q1'
     estado_final = 'q8'
 
-    cadena = input("Ingrese un genoma, cromosoma o gen para analizar:")
+    # Obtener la cadena ingresada por el usuario
+    # 
+    # 
+    # cadena = input("Ingrese un genoma, cromosoma o gen para analizar: ")
+    
+    # Extraer la cadena de una archivo FASTA
+    # 
+    # 
+    # Buscar el archivo
+    x = open(r'D:\proyectos\sequence.fasta','r')
+    # Obtener los datos
+    a = x.read()
+    # Eliminar la primera linea
+    b = a[47:]
+    # Eliminar los saltos de linea
+    c = b.replace("\n","")
+
+    # Extraer el gen F8 del cromosoma X
+    inicio  = "tgaggagtacaagagtag"
+    inicio_upper = inicio.upper()
+    final = 'ctcgccgccccaagcactt'
+    final_upper = final.upper()
+    B_index = c.find(inicio_upper)
+    E_index = c.find(final_upper)
+
+    cadena = c[B_index:E_index]
+    
     estado_actual = {estado_inicial}
 
     for simbolo in cadena:

f2d4126c6e5223bf5d2a27b4b5bbd3be01f9eaf3
Carlos Martin Quispe Bartolo <104172383+tripalidex@users.noreply.github.com>
2023-11-26 17:28:53 -0500

    Versión Final del autómata
    implementación de la eliminación de la primera fila del archivo .fasta

--- codigo.py
+++ codigo.py
@@ -79,22 +79,30 @@ def main():
     estado_inicial = 'q1'
     estado_final = {'q8'}    
 
-    #cadena = input("Ingrese una cadena con los digitos 0, 1, 2 o 3 en cualquier orden, para evaluar: ").upper()
+    # cadena = input("Ingrese una cadena con los digitos 0, 1, 2 o 3 en cualquier orden, para evaluar: ").upper()
 
-    # Extraer la cadena de un archivo FASTA
-    # Buscar el archivo
-    #archivo = open(r'D:\proyectos\sequence.fasta', 'r')
-    #para los demas usen el de arriba
-    archivo = open(r'/Users/angellollerena/Documents/GitHub/TeoriaComputacion/sequence.fasta','r')
-    # Obtener los datos
-    datos = archivo.read()
-    # Eliminar la primera línea
-    datos_sin_1_linea = datos[47:]
-    # Eliminar los saltos de línea
-    # Obtener la cadena completa
-    cadena = datos_sin_1_linea.replace("\n", "")
+    # Leer el archivo FASTA y separar el encabezado de la secuencia
+    with open(r'D:\proyectos\sequence.fasta', 'r') as archivo:
+        encabezado = ""
+        secuencia = ""
+        en_encabezado = True
 
-    #Agrupa cadenas de 3 
+        for linea in archivo:
+            if en_encabezado:
+                encabezado += linea.strip() + "\n"
+                if linea.strip().endswith('.') or linea.strip()[-1].islower():
+                    en_encabezado = False
+            else:
+                secuencia += linea.strip()
+
+    # ESTO SIRVE PARA VERIFICAR QUE ESTAMOS LEYENDO CON LA FUNCION agrupar_en_tres():
+    # Abrir el archivo en modo de escritura
+    with open('D:/proyectos/archivo.txt', 'w') as archivo:
+        # Escribir el contenido de la variable en el archivo
+        archivo.write(secuencia)
+
+    secuencia += '1'
+    # Agrupa cadenas de 3 
     def agrupar_en_tres(cadena):
         conjuntos = [cadena[i:i+3] for i in range(0, len(cadena), 3)]
         if len(conjuntos[-1]) == 1:
@@ -102,22 +110,26 @@ def main():
             conjuntos.pop()
         return conjuntos
 
-    conjuntos = agrupar_en_tres(cadena)
-
+    conjuntos = agrupar_en_tres(secuencia)
+    
     # Mostrar los conjuntos
+    esValida = True
     for conjunto in conjuntos:
-        print(conjunto)
         estado_actual = {estado_inicial}
         for simbolo in conjunto:
             nuevos_estados = set()
             for estado in estado_actual:
                 nuevos_estados.update(transiciones(estado, simbolo))
-                estado_actual = nuevos_estados
+            estado_actual = nuevos_estados
 
-        if estado_final.intersection(estado_actual):
-            print("La cadena es válida")
-        else:
-            print("La cadena NO es válida.")
+        if not estado_final.intersection(estado_actual):
+            esValida = False
+            break
+
+    if esValida:
+        print("La cadena completa es válida")
+    else:
+        print("La cadena completa NO es válida.")
 
 if __name__ == "__main__":
     main()
